053c8397165cc332006c64c945679810370bca5d
tommycapello <thomcapello@gmail.com>
2021-07-16 11:07:47 -0400

    setting up routers

--- api/server.js
+++ api/server.js
@@ -1,7 +1,14 @@
 // build your server here and require it from index.js
 const express = require('express')
 const server = express()
+const projectRouter = require('./project/router')
+const resourceRouter = require('./resource/router')
+const taskRouter = require('./task/router')
 
 server.use(express.json())
+
+server.use('/api/project', projectRouter)
+server.use('/api/resource', resourceRouter)
+server.use('/api/task', taskRouter)
 
 module.exports = server
--- index.js
+++ index.js
@@ -2,4 +2,4 @@
 const server = require('./api/server')
 const PORT = process.env.PORT || 5000
 
-server.listen(PORT, console.log('listening on port', PORT))
+server.listen(PORT, () => console.log('listening on port', PORT))
