831f97bba68b7bcf92c4f1fc349405b55f45d307
tommycapello <thomcapello@gmail.com>
2021-07-16 14:12:26 -0400

    get methods working

--- api/project/router.js
+++ api/project/router.js
@@ -2,16 +2,17 @@
 const router = require('express').Router()
 const Project = require('./model')
 
-router.get('/', (req, res, next) => {
-    Project.getProjects()
-    .then(projects => {
-        res.json(projects)
-    })
-    .catch(next)
+router.get('/', async(req, res, next) => {
+    try{
+        const rows = await Project.getProjects()
+        res.json(rows)
+    }
+    catch(err){
+        next(err)
+    }
 })
 
 router.post('/', (req, res, next) => {
-
 })
 
 
